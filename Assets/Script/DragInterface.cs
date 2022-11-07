94db0fc02ffa1d7d5f33c55eebfbbdece967cbbc
PabloMataGamez <pablo.mata.gamez@student.howest.be>
2022-11-07 21:39:48 +0100

    Interface 1.0

--- Assets/Script/DragInterface.cs
+++ Assets/Script/DragInterface.cs
@@ -1,6 +1,8 @@
+using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 [RequireComponent(typeof(Image))]
 public class DragInterface : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
@@ -14,9 +16,33 @@ public class DragInterface : MonoBehaviour, IBeginDragHandler, IDragHandler, IEn
     private GameObject m_DraggingIcon;
     private RectTransform m_DraggingPlane;
 
+    private int _cardsInTable = 0;
+    private int _totalCards;
+
+    [SerializeField]
+    private int _maxTotalCards = 12;
+    [SerializeField]
+    private int _maxCardsInTable = 5;
+
+    [SerializeField]
+    private GameObject _card1;
+    [SerializeField]
+    private GameObject _card2;
+    [SerializeField]
+    private GameObject _card3;
+    [SerializeField]
+    private GameObject _card4;
+
+    [SerializeField]
+    private Canvas _canvasParent;
+
+
     private void Awake()
     {
         _camera = Camera.main;
+        _canvasParent = FindObjectOfType<Canvas>();
+
+         AddNewCard();
     }
     public void OnBeginDrag(PointerEventData eventData)
     {
@@ -36,7 +62,7 @@ public class DragInterface : MonoBehaviour, IBeginDragHandler, IDragHandler, IEn
 
         image.sprite = GetComponent<Image>().sprite;
 
-
+        //Just to make sure its on place
         if (dragOnSurfaces)
             m_DraggingPlane = transform as RectTransform;
         else
@@ -45,7 +71,7 @@ public class DragInterface : MonoBehaviour, IBeginDragHandler, IDragHandler, IEn
         SetDraggedPosition(eventData);
     }
 
-    public void OnDrag(PointerEventData data)
+    public void OnDrag(PointerEventData data) //Make the image follow the mouse
     {
         if (m_DraggingIcon != null)
             SetDraggedPosition(data);
@@ -72,9 +98,13 @@ public class DragInterface : MonoBehaviour, IBeginDragHandler, IDragHandler, IEn
 
         if (IsOverCube())
         {
+            _cardsInTable--;
             Destroy(this.gameObject);
+
+            AddNewCard();
         }
     }
+
 
     static public T FindInParents<T>(GameObject go) where T : Component
     {
@@ -99,5 +129,40 @@ public class DragInterface : MonoBehaviour, IBeginDragHandler, IDragHandler, IEn
         if (Physics.Raycast(ray, _camera.farClipPlane, _cubeLayer))
             return true;
         return false;
+    }
+
+
+    private void AddNewCard()
+    {
+        if (_totalCards < _maxTotalCards && _cardsInTable < _maxCardsInTable)
+        {
+            int random = Random.Range(1, 4);
+
+            if (random == 1)
+            {
+                var newCard = Instantiate(_card1, new Vector3(transform.position.x, transform.position.y, transform.position.z), 
+                    Quaternion.identity, _canvasParent.transform);
+            }
+            else if (random == 2)
+            {
+                var newCard = Instantiate(_card1, new Vector3(transform.position.x, transform.position.y, transform.position.z), 
+                    Quaternion.identity, _canvasParent.transform);
+
+            }
+            else if (random == 3)
+            {
+                var newCard = Instantiate(_card1, new Vector3(transform.position.x, transform.position.y, transform.position.z), 
+                    Quaternion.identity, _canvasParent.transform);
+
+            }
+            else if (random == 4)
+            {
+                var newCard = Instantiate(_card1, new Vector3(transform.position.x, transform.position.y, transform.position.z), 
+                    Quaternion.identity, _canvasParent.transform);
+
+            }
+            _totalCards++;
+            _cardsInTable++;
+        }
     }
 }
